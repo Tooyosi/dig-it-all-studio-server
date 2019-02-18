a38080e1aa05d5a02ff7d0aef4ff3f205ddb229f
Tooyosi <oluwatoyosi.oyegoke@gmail.com>
2019-02-18 12:13:29 +0100

    made functional for react frontend

--- server.js
+++ server.js
@@ -1,51 +1,36 @@
-// var express = require('express'),
-var restify = require("restify"),
+var express = require('express'),
+// var restify = require("restify"),
     nodemailer = require("nodemailer"),
     bodyParser = require("body-parser"),
-    corsMiddleware = require("restify-cors-middleware");
+    corsMiddleware = require("restify-cors-middleware")
+    app         = express();
     
     // require('dotenv').config();
-//Middleware Cors
-var cors = corsMiddleware({
-    preflightMaxAge: 5,
-    origins: ['*'],
-    allowHeaders: ['API-Token'],
-    exposeHeaders: ['Api-Token-Expiry']
-});
 
+var cors = require('cors');
+app.use(cors({credentials: true, origin: 'http://localhost:8080'}));
 
-// Server
-var server = restify.createServer();
-// handle cross-origin problem
-server.pre(cors.preflight);
-server.use(cors.actual);
-// query and bodyparse to use data sent by url
-server.use(restify.plugins.queryParser({
-    mapParams: true
-}));
-server.use(restify.plugins.bodyParser({
-    mapParams: true
-}));
-server.use(restify.plugins.fullResponse());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // setup email config
 var smtpTransport = nodemailer.createTransport({
     service: "Gmail",
     auth: {
-        user: "tuc0476@gmail.com",
-        pass: "Conjugate1"
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
     }
 });
 
-server.get("/", function(req, res){
+app.get("/", function(req, res){
     res.send("Running app");
 })
 
-server.get("/success", function(req, res){
+app.get("/success", function(req, res){
     res.send("Mail sent! We'll get back to you shortly ");
 })
 var result = {"data": ""};
-server.post("/api/email", function create(req, res, next){
+app.post("/api/email", function create(req, res, next){
     // console.log(req.params)
     console.log(req.body)
     var mail = {
@@ -59,21 +44,21 @@ server.post("/api/email", function create(req, res, next){
             result.data ="An error occured, Please Try Again"
             console.log(result);
             console.log(error);
-            res.send(JSON.stringify(result)); 
+            return res.status(400).send(JSON.stringify(result)); 
         }else{
             result.data ="Mail sent! We'll get back to you shortly "
             console.log(result);
             console.log("Success")
-            res.send(JSON.stringify(result));                        
+            return res.status(200).send(JSON.stringify(result));                        
         }
         smtpTransport.close();
     });
 
-    res.send(201, req.params)
+    // return res.status(201).send(req.params)
 })
 
 
 
-server.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log(`app is working on ${process.env.PORT}`)
 })
